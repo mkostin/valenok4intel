cc5e8bb7753d72e56a207a6886f94940676b8d7b
Max Kostin <dev@maxkostin.ru>
2015-05-20 23:54:15 +0300

    Initial commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace emotion_viewer.cs
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Start = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            this.sourceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.moduleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MainMenu = new System.Windows.Forms.MenuStrip();
+            this.modeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Live = new System.Windows.Forms.ToolStripMenuItem();
+            this.Playback = new System.Windows.Forms.ToolStripMenuItem();
+            this.Record = new System.Windows.Forms.ToolStripMenuItem();
+            this.LocationCheck = new System.Windows.Forms.CheckBox();
+            this.Status2 = new System.Windows.Forms.StatusStrip();
+            this.StatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.Scale2 = new System.Windows.Forms.CheckBox();
+            this.Panel2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.MainMenu.SuspendLayout();
+            this.Status2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Panel2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Start.Location = new System.Drawing.Point(769, 185);
+            this.Start.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(107, 28);
+            this.Start.TabIndex = 2;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // Stop
+            // 
+            this.Stop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Stop.Enabled = false;
+            this.Stop.Location = new System.Drawing.Point(769, 221);
+            this.Stop.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(107, 28);
+            this.Stop.TabIndex = 3;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // sourceToolStripMenuItem
+            // 
+            this.sourceToolStripMenuItem.Name = "sourceToolStripMenuItem";
+            this.sourceToolStripMenuItem.Size = new System.Drawing.Size(66, 24);
+            this.sourceToolStripMenuItem.Text = "Device";
+            // 
+            // moduleToolStripMenuItem
+            // 
+            this.moduleToolStripMenuItem.Name = "moduleToolStripMenuItem";
+            this.moduleToolStripMenuItem.Size = new System.Drawing.Size(72, 24);
+            this.moduleToolStripMenuItem.Text = "Module";
+            // 
+            // MainMenu
+            // 
+            this.MainMenu.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.MainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sourceToolStripMenuItem,
+            this.moduleToolStripMenuItem,
+            this.modeToolStripMenuItem});
+            this.MainMenu.Location = new System.Drawing.Point(0, 0);
+            this.MainMenu.Name = "MainMenu";
+            this.MainMenu.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
+            this.MainMenu.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.MainMenu.Size = new System.Drawing.Size(888, 28);
+            this.MainMenu.TabIndex = 0;
+            this.MainMenu.Text = "MainMenu";
+            // 
+            // modeToolStripMenuItem
+            // 
+            this.modeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Live,
+            this.Playback,
+            this.Record});
+            this.modeToolStripMenuItem.Name = "modeToolStripMenuItem";
+            this.modeToolStripMenuItem.Size = new System.Drawing.Size(60, 24);
+            this.modeToolStripMenuItem.Text = "Mode";
+            // 
+            // Live
+            // 
+            this.Live.Checked = true;
+            this.Live.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.Live.Name = "Live";
+            this.Live.Size = new System.Drawing.Size(136, 26);
+            this.Live.Text = "Live";
+            this.Live.Click += new System.EventHandler(this.Live_Click);
+            // 
+            // Playback
+            // 
+            this.Playback.Name = "Playback";
+            this.Playback.Size = new System.Drawing.Size(136, 26);
+            this.Playback.Text = "Playback";
+            this.Playback.Click += new System.EventHandler(this.Playback_Click);
+            // 
+            // Record
+            // 
+            this.Record.Name = "Record";
+            this.Record.Size = new System.Drawing.Size(136, 26);
+            this.Record.Text = "Record";
+            this.Record.Click += new System.EventHandler(this.Record_Click);
+            // 
+            // LocationCheck
+            // 
+            this.LocationCheck.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LocationCheck.AutoSize = true;
+            this.LocationCheck.Checked = true;
+            this.LocationCheck.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.LocationCheck.Location = new System.Drawing.Point(774, 65);
+            this.LocationCheck.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.LocationCheck.Name = "LocationCheck";
+            this.LocationCheck.Size = new System.Drawing.Size(84, 21);
+            this.LocationCheck.TabIndex = 19;
+            this.LocationCheck.Text = "Location";
+            this.LocationCheck.UseVisualStyleBackColor = true;
+            // 
+            // Status2
+            // 
+            this.Status2.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.Status2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StatusLabel});
+            this.Status2.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.Flow;
+            this.Status2.Location = new System.Drawing.Point(0, 545);
+            this.Status2.Name = "Status2";
+            this.Status2.Padding = new System.Windows.Forms.Padding(1, 0, 19, 0);
+            this.Status2.Size = new System.Drawing.Size(888, 25);
+            this.Status2.TabIndex = 25;
+            this.Status2.Text = "Status2";
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(29, 20);
+            this.StatusLabel.Text = "OK";
+            // 
+            // Scale2
+            // 
+            this.Scale2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Scale2.AutoSize = true;
+            this.Scale2.Checked = true;
+            this.Scale2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.Scale2.Location = new System.Drawing.Point(775, 33);
+            this.Scale2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Scale2.Name = "Scale2";
+            this.Scale2.Size = new System.Drawing.Size(65, 21);
+            this.Scale2.TabIndex = 26;
+            this.Scale2.Text = "Scale";
+            this.Scale2.UseVisualStyleBackColor = true;
+            // 
+            // Panel2
+            // 
+            this.Panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Panel2.ErrorImage = null;
+            this.Panel2.InitialImage = null;
+            this.Panel2.Location = new System.Drawing.Point(16, 256);
+            this.Panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Panel2.Name = "Panel2";
+            this.Panel2.Size = new System.Drawing.Size(701, 276);
+            this.Panel2.TabIndex = 27;
+            this.Panel2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(51, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 33);
+            this.button1.TabIndex = 28;
+            this.button1.Text = "Learn";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(51, 93);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(299, 155);
+            this.textBox1.TabIndex = 29;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(228, 53);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 34);
+            this.button2.TabIndex = 30;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(382, 52);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(122, 34);
+            this.button3.TabIndex = 31;
+            this.button3.Text = "Enter key";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(382, 94);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(299, 155);
+            this.textBox2.TabIndex = 32;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(888, 570);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Panel2);
+            this.Controls.Add(this.Scale2);
+            this.Controls.Add(this.Status2);
+            this.Controls.Add(this.LocationCheck);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.MainMenu);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Intel(R) RealSense(TM) SDK: Emotions Viewer";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.MainMenu.ResumeLayout(false);
+            this.MainMenu.PerformLayout();
+            this.Status2.ResumeLayout(false);
+            this.Status2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Panel2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.ToolStripMenuItem sourceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem moduleToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip MainMenu;
+        private System.Windows.Forms.CheckBox LocationCheck;
+        private System.Windows.Forms.StatusStrip Status2;
+        private System.Windows.Forms.ToolStripStatusLabel StatusLabel;
+        private System.Windows.Forms.CheckBox Scale2;
+        private System.Windows.Forms.PictureBox Panel2;
+        private System.Windows.Forms.ToolStripMenuItem modeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem Live;
+        private System.Windows.Forms.ToolStripMenuItem Playback;
+        private System.Windows.Forms.ToolStripMenuItem Record;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
